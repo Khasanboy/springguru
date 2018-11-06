2435036332489003b8479ef173f6a2c3549d95d3
Khasanboy <akbarov8808@gmail.com>
2018-11-07 00:06:53 +0200

    Thymeleaf added

---
+++ spring guru course 1/src/main/java/com/monica/jpaexample/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package com.monica.jpaexample.controller;
+
+import com.monica.jpaexample.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    private String getAuthors(Model model){
+        model.addAttribute("authors", this.authorRepository.findAll());
+
+        return "authors";
+    }
+
+}
