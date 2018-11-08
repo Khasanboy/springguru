96c4f8ee652f753f9c4c4f3a88b8a790c1802ddb
Khasanboy <akbarov8808@gmail.com>
2018-11-08 22:22:38 +0200

    Default profiling

---
+++ di-demo/src/main/java/com/monica/didemo/services/PrimaryDefaultGermanGreetingServiceImpl.java
@@ -0,0 +1,16 @@
+package com.monica.didemo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile({"de", "default"})
+public class PrimaryDefaultGermanGreetingServiceImpl implements  GreetingService {
+    @Override
+    public String greet() {
+        System.out.println("Greeting from default German service");
+        return "default";
+    }
+}
